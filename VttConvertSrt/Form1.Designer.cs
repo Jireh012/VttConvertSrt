f5e0469fc35b6ed73ed171109fa34a66b25094e9
Jireh <jeanxyz.012@gmail.com>
2020-11-13 16:10:58 +0800

    first commit

---
+++ VttConvertSrt/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace VttConvertSrt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.author = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("黑体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(7, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vtt转Srt";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("黑体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(7, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Srt转Vtt";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(120, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "选择.vtt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_Choose_VTT);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "转换";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_Transfer_VTT);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(120, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "选择.srt";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_Choose_SRT);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(209, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "转换";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn_Transfer_SRT);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // author
+            // 
+            this.author.AutoSize = true;
+            this.author.Font = new System.Drawing.Font("楷体", 10F);
+            this.author.Location = new System.Drawing.Point(117, 103);
+            this.author.Name = "author";
+            this.author.Size = new System.Drawing.Size(98, 14);
+            this.author.TabIndex = 6;
+            this.author.Text = "Author：Jireh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("楷体", 10F);
+            this.label3.Location = new System.Drawing.Point(6, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 14);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Version：1.0.0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("楷体", 10F);
+            this.label4.Location = new System.Drawing.Point(221, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 14);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "lyile.cn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 126);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.author);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.Name = "Form1";
+            this.Text = "字幕格式转换器        By：Jireh";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label author;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VttConvertSrt/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace VttConvertSrt
+{
+    public partial class Form1 : Form
+    {
+        private string[] fileNames;//所有选定文件文件名(包含文件路径)
+        private string vttPrefix = "WEBVTT\n";// vtt前缀有多种格式，youtube需要自行修改
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btn_Choose_VTT(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "请选择.vtt文件";
+            // 能够多选
+            openFileDialog.Multiselect = true;
+            // 格式过滤器
+            openFileDialog.Filter = "vtt字幕文件(*.vtt)|*.vtt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileNames = openFileDialog.FileNames;// 得到所有选定文件文件名(包含文件路径)
+            }
+        }
+
+
+        private void btn_Transfer_VTT(object sender, EventArgs e)
+        {
+            foreach (string fileName in fileNames)
+            {
+                vtt2srt(fileName);
+            }
+            MessageBox.Show("Convert Success!");
+        }
+
+
+        private void btn_Choose_SRT(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "请选择.srt文件";
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "srt字幕文件(*.srt)|*.srt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileNames = openFileDialog.FileNames;
+            }
+        }
+
+        private void btn_Transfer_SRT(object sender, EventArgs e)
+        {
+            foreach (string fileName in fileNames)
+            {
+                srt2vtt(fileName);
+            }
+            MessageBox.Show("Convert Success!");
+        }
+
+
+        private void vtt2srt(string name)
+        {
+            string[] oldTexts = File.ReadAllLines(name, Encoding.UTF8);
+            List<string> resultTextsList = new List<string>();
+
+            // 匹配两种时间格式          vtt                                  srt
+            // 分:秒.毫秒      00:03.490 --> 00:03.950           00:00:03.490 --> 00:00:03.950
+            // 时:分:秒.毫秒   01:00:03.490 --> 01:00:03.950     01:00:03.490 --> 01:00:03.950
+            //string timePattern = @"(\d{2}:)?(\d{2}:)(\d{2}).(\d{3})\s-->\s(\d{2}:)?(\d{2}:)(\d{2}).(\d{3})";
+            string timePattern1 = @"(\d{2}:)(\d{2}).(\d{3})\s-->\s(\d{2}:)(\d{2}).(\d{3})";
+            string timePattern2 = @"(\d{2}:)(\d{2}:)(\d{2}).(\d{3})\s-->\s(\d{2}:)(\d{2}:)(\d{2}).(\d{3})";
+            int lineNumber = 1;
+            bool isFirstMatchTimeLine = false;// 是否第一次匹配到时间轴
+
+            foreach (string oldLine in oldTexts)
+            {
+                if (Regex.IsMatch(oldLine, timePattern1) || Regex.IsMatch(oldLine, timePattern2))
+                {
+                    // vtt2srt 加行号
+                    string lineNumberText = lineNumber.ToString();
+                    resultTextsList.Add(lineNumberText);
+                    lineNumber++;
+                    // vtt2srt '.'改为','
+                    if (Regex.IsMatch(oldLine, timePattern1))
+                    {
+                        resultTextsList.Add(Regex.Replace(oldLine, timePattern1, "00:$1$2,$3 --> 00:$4$5,$6"));
+                    }
+                    else if (Regex.IsMatch(oldLine, timePattern2))
+                    {
+                        resultTextsList.Add(Regex.Replace(oldLine, timePattern2, "$1$2$3,$4 --> $5$6$7,$8"));
+                    }
+                    isFirstMatchTimeLine = true;
+                }
+                else
+                {
+                    // 忽略第一个时间轴之前所有的内容
+                    // 未匹配到第一个行号，isFirstMatchTimeLine始终为false，不添加到List
+                    if (isFirstMatchTimeLine == true)
+                    {
+                        resultTextsList.Add(oldLine);
+                    }
+                }
+            }
+
+            string newName = name.Replace(".vtt", ".srt");
+            FileInfo fileInfo = new FileInfo(newName);
+            if (fileInfo.Exists)
+            {
+                newName = newName.Replace(".srt", " - 副本.srt");
+            }
+            File.WriteAllLines(newName, resultTextsList, Encoding.UTF8);
+        }
+
+
+        private void srt2vtt(string name)
+        {
+            string[] oldTexts = File.ReadAllLines(name, Encoding.UTF8);
+            List<string> resultTextsList = new List<string>();
+            string timePattern = @"(\d{2}:)(\d{2}:)(\d{2}),(\d{3})\s-->\s(\d{2}:)(\d{2}:)(\d{2}),(\d{3})";
+            string lineNumberPattern = @"^\d+$";// 匹配行号(一到多位纯数字)
+            bool isFirstMatchTimeLine = false;// 是否第一次匹配到时间轴
+
+            foreach (string oldLine in oldTexts)
+            {
+                if (Regex.IsMatch(oldLine, timePattern))
+                {
+                    if (isFirstMatchTimeLine == false)
+                    {
+                        resultTextsList.Add(vttPrefix);// 在第一个时间轴前面添加"WEBVTT\n"前缀
+                    }
+                    resultTextsList.Add(Regex.Replace(oldLine, timePattern, "$1$2$3.$4 --> $5$6$7.$8"));
+                    isFirstMatchTimeLine = true;
+                }
+                else if (!Regex.IsMatch(oldLine, lineNumberPattern))// 忽略行号和时间轴，剩下字幕文本
+                {
+                    resultTextsList.Add(oldLine);
+                }
+            }
+
+            string newName = name.Replace(".srt", ".vtt");
+            FileInfo fileInfo = new FileInfo(newName);
+            if (fileInfo.Exists)
+            {
+                newName = newName.Replace(".vtt", " - 副本.vtt");// 更改文件名字符串
+            }
+            File.WriteAllLines(newName, resultTextsList, Encoding.UTF8);
+        }
+    }
+}
